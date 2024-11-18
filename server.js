414eb5b943c3bb0b50c892cbbf838dfbc26095f6
Dhanishetty-Ganesh <varmaganesh224@gmail.com>
2024-11-18 23:06:48 +0530

    changed server.js added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors"); // Import the CORS package
 const connectDB = require("./config/db");
 const transactionRoutes = require("./routes/transactionRoutes");
 const errorHandler = require("./middleware/errorHandler");
@@ -8,8 +9,16 @@ dotenv.config();
 
 const app = express();
 
+// Enable CORS for all origins (no need to specify the frontend URL)
+app.use(cors());
+
 // Middleware
 app.use(express.json());
+
+// Hello World Route
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 // Routes
 app.use("/api", transactionRoutes);
